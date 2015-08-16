7301dd92f58440305a85e7d577f5d23b5509d394
Amol Mandhane <manamol296@gmail.com>
2015-08-16 07:11:15 +0530

    Solve autodoc PyQt4 dependency issues

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas'
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas',
                 'PyQt4', 'sip']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
