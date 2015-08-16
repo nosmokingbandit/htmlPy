4f5cea32113c8f075e540fdc44e7e61de7bd9d82
Amol Mandhane <manamol296@gmail.com>
2015-08-16 07:13:25 +0530

    Solve autodoc PyQt4 dependency issue

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ class Mock(MagicMock):
         return Mock()
 
 MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas',
-                'PyQt4', 'sip']
+                'PyQt4', 'sip', 'PyQt4.QtCore']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
